7155c173d58f2037916f97055b1bbae876ed9a4a
Alexander Ponomarev <a.a.ponomarev.software@gmail.com>
2026-03-05 22:08:49 +0300

    refactor(optimizers/__init__): English module docstring

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,40 +1,25 @@
-"""
-Оптимизаторы и вспомогательные инструменты для машинного обучения.
+"""Optimizers and utilities for ML (GD, SGD, Adam, AdamW, Lion, etc.)."""
 
-Этот модуль включает в себя реализации популярных оптимизаторов, таких как:
-- Стандартный градиентный спуск (Gradient Descent)
-- Стохастический градиентный спуск (SGD)
-- RMSProp
-- AdamW
-- Lion
-- Adan
-- MARS
-- Adagrad
-- Sophia
-- AMSGrad
-- LARS
-- Adam
-
-Также присутствует вспомогательная функция `timed`, которая используется для замера времени выполнения операций.
-
-Каждый оптимизатор реализован как класс, и их можно использовать для настройки параметров обучения нейронных сетей или других моделей машинного обучения.
-"""
-
-from .SGD import StochasticGradientDescent
-from .GD import GradientDescent
-from .RMSp import RMSProp
-from .Aw import AdamW
-from .L import Lion
-from .An import Adan
-from .Make_vAriance_Reduction_Shine import MARS
-from .Agrad import Adagrad
-from .Sophia import Sophia
-from .AMSGrad import AMSGrad
-from .LARS import LARS
-from .Adaptive import Adam
-from .dtime import timed 
+from .sgd import StochasticGradientDescent
+from .gd import GradientDescent
+from .rmsprop import RMSProp
+from .adamw import AdamW
+from .lion import Lion
+from .adan import Adan
+from .mars import MARS
+from .adagrad import Adagrad
+from .sophia import Sophia
+from .amsgrad import AMSGrad
+from .lars import LARS
+from .adam import Adam
+from .base import BaseOptimizer, clip_gradient
+from .dtime import timed
+from .registry import get_optimizer_names, get_param_spec, create_optimizer, OPTIMIZER_REGISTRY
 
 __all__ = [
+    "BaseOptimizer",
+    "clip_gradient",
+    "timed",
     "StochasticGradientDescent",
     "GradientDescent",
     "RMSProp",
@@ -46,5 +31,9 @@ __all__ = [
     "Sophia",
     "AMSGrad",
     "LARS",
-    "Adam"
+    "Adam",
+    "get_optimizer_names",
+    "get_param_spec",
+    "create_optimizer",
+    "OPTIMIZER_REGISTRY",
 ]
